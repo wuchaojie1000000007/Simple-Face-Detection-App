7210ff7c9bcd020174563f00e250fa2ed8f7af7e
Chaojie Wu <wuchaojie925@gmail.com>
2021-05-07 15:25:56 +0800

    add imageLabeler to the app

--- app/src/main/java/com/example/ml/MainActivity.kt
+++ app/src/main/java/com/example/ml/MainActivity.kt
@@ -9,7 +9,10 @@ import com.example.ml.databinding.ActivityMainBinding
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val imageView by lazy { binding.imageView }
     private val predictButton by lazy { binding.predictButton }
+    private val labelButton by lazy { binding.labelButton }
+    private val labelText by lazy { binding.labelText }
     private val fileName = "man.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,19 +29,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Get bitmap from assets folder
         val bitmap: Bitmap? = assetToBitmap(fileName)
-        bitmap?.apply {
-            imageView.setImageBitmap(this)
-        }
+
+        // Use the bitmap to set imageView
+        bitmap?.apply { imageView.setImageBitmap(this) }
+
+        // Get InputImage for the ML model from the bitmap
+        val image = InputImage.fromBitmap(bitmap!!, 0)
+
+        // Get Face Detection model
+        val faceDetector = getFaceDetector()
 
         predictButton.setOnClickListener {
-            val faceDetectorOptions = FaceDetectorOptions.Builder()
-                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
-                .build()
-            val detector = FaceDetection.getClient(faceDetectorOptions)
-            val image = InputImage.fromBitmap(bitmap!!, 0)
-
-            val result = detector.process(image)
+            val result = faceDetector.process(image)
                 .addOnSuccessListener {
                     bitmap.apply {
                         imageView.setImageBitmap(drawWithRectangle(it))
@@ -45,9 +51,22 @@ class MainActivity : AppCompatActivity() {
                     Log.d("MainActivity", "$it")
                 }
         }
+
+        // Get Image Labeling model
+        val imageLabeler = getImageLabeler()
+
+        labelButton.setOnClickListener {
+            val result = imageLabeler.process(image)
+                .addOnSuccessListener {
+                    labelText.text = it[0].text
+                    Log.d("MainActivity", "${it[0]}")
+                }.addOnFailureListener {
+                    Log.d("MainActivity", "$it")
+                }
+        }
     }
 
-    private fun Bitmap.drawWithRectangle(faces: List<Face>): Bitmap? {
+    private fun Bitmap.drawWithRectangle(faces: List<Face>): Bitmap {
         val bitmap = copy(config, true)
         val canvas = Canvas(bitmap)
         faces.forEach {
@@ -67,13 +86,18 @@ class MainActivity : AppCompatActivity() {
         return try {
             assets.open(fileName).let {
                 BitmapFactory.decodeStream(it)
-
             }
-            /*with(assets.open(fileName)) {
-                BitmapFactory.decodeStream(this)
-            }*/
         } catch (e: IOException) {
             null
         }
     }
+
+    private fun getFaceDetector(): FaceDetector {
+        val faceDetectorOptions = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .build()
+        return FaceDetection.getClient(faceDetectorOptions)
+    }
+
+    private fun getImageLabeler() = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
 }
